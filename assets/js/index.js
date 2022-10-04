0ee0d23530267b6342862ffc6cb5bff7b808f208
Priscila Passos <cylla_@hotmail.com>
2022-10-04 17:36:42 -0300

    botoes_add_exclui

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,85 @@
+//referencias html
 let atividade = document.querySelector("#atividade");
 let horario = document.querySelector("#horario");
-let horario_input = document.querySelector("#horario_input");
 let dia = document.querySelector("#dias-da-semana");
-let listaAtividade = [];
 let btAdicionar = document.querySelector(".principal__bt-adicionar");
 let btexcluirTodos = document.querySelector(".principal__bt-excluir-todos")
 let btsegunda = document.querySelector("#segunda");
 let btterca = document.querySelector("#terca");
 let containerAtividades = document.querySelector(".atividades");
+
+let diaSelecionado;
+
+let agenda ={
+        segunda:[{desc:"estudos daily", hora: "10:11", id:1},{desc:"estudos", hora: "11:11", id:2}],
+        terca:[],
+        quarta:[],
+        quinta:[],
+        sexta:[],
+        sabado:[],
+        domingo:[],
+
+}
+
+function addAtividade(atv, dia){
+    agenda[dia].push(atv)
+
+}
+
+function pegarDia(dia){
+    
+    containerAtividades.innerHTML = '';
+    
+    agenda[dia].forEach((item)=>{
+       
+        console.log(item);
+        let atividadesItens = document.createElement("div");
+        let horarioContainer = document.createElement("div");
+        let atividadeContainer = document.createElement("div");
+        
+        horarioContainer.classList.add("planner__horario-um");
+        atividadeContainer.classList.add("atividades__planner");
+
+        horarioContainer.innerHTML=item.hora;
+        atividadeContainer.innerHTML=item.desc;
+
+        containerAtividades.appendChild(atividadesItens);
+        atividadesItens.appendChild(horarioContainer);
+        atividadesItens.appendChild(atividadeContainer);
+     })
+    
+}
+
+btAdicionar.addEventListener("click", function(e){
+    e.preventDefault();
+   
+    let descricao = atividade.value;
+    let hours = horario.value;
+    let dias = dia.value;
+
+    let tarefa = {
+       desc:descricao,
+       hora:hours,
+    }
+    addAtividade(tarefa, dias);
+    
+})
+
+btsegunda.addEventListener("click", function(e){
+    e.preventDefault();
+    let dia = e.target.id;
+    diaSelecionado = dia;
+    pegarDia(dia);
+    
+})
+
+btexcluirTodos.addEventListener('click', function(e){
+    e.preventDefault();
+    agenda[diaSelecionado] = [];
+    pegarDia(diaSelecionado);
+})
+
+
 const data = new Date();
 const meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
 const ano = data.getFullYear();
@@ -15,62 +87,9 @@ const mes = meses[data.getMonth()];
 const diaAtual = data.getDate();
 const hora = data.getHours();
 const minutos = data.getMinutes();
-atividade="block";
-horario ="block";
+let listaAtividade = [];
 const zeroFill = n => {
     return ('0' + n).slice(-2);
 }
     document.getElementById("hora").innerHTML = zeroFill(hora) + " : " + zeroFill(minutos);
     document.getElementById("data").innerHTML = zeroFill(diaAtual) + " de " + mes + " de " + ano;
-
-    
-btAdicionar.addEventListener("click", function(e){
-    e.preventDefault();
-   
-    listaAtividade.push({
-    atividade:atividade.value, 
-    horario:horario.value,
-    horario_input:horario_input.value,
-    dia:dia.value
-});
-        console.log(listaAtividade)
-    atividade.value ="";
-    
-})
-
-
-
-
-btexcluirTodos.addEventListener("click", function(e){
-    e.preventDefault();
-    listaAtividade.splice(0, listaAtividade.length);
-    
-    
-
-    })
-
-
-
-btsegunda.addEventListener("click", function(e){
-    e.preventDefault();
-    containerAtividades.innerHTML="";
-    listaAtividade.forEach((item)=>{
-       
-        console.log(item.atividade);
-        let atividadesItens = document.createElement("div");
-        let horarioContainer = document.createElement("div");
-        let atividadeContainer = document.createElement("div");
-    
-        horarioContainer.classList.add("planner__horario-um");
-        atividadeContainer.classList.add("atividades__planner");
-
-        horarioContainer.innerHTML=item.horario;
-        atividadeContainer.innerHTML=item.atividade;
-
-        containerAtividades.appendChild(atividadesItens);
-        atividadesItens.appendChild(horarioContainer);
-        atividadesItens.appendChild(atividadeContainer);
-    })
-
-})
-
